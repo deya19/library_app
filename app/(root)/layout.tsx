f381e01d9bfe0fcbea0322e8c8bc18a610da90c2
deya19 <deyasaleh2@gmail.com>
2025-01-31 23:36:21 +0300

    implement qstash  workflows for user onboarding

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,36 @@
 import { auth } from "@/auth";
 import Header from "@/components/Header";
+import { db } from "@/database/db";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 import { ReactNode } from "react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
-    const session = await auth();
-  
-    if (!session) redirect("/sign-in");
+  const session = await auth();
+
+  if (!session) redirect("/sign-in");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    // get the user and see if the last activity data is today 
+    const user = await db.
+    select().from(users)
+    .where(eq(users.id, session?.user?.id)).limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10)) return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
-        <Header session={session}/>
+        <Header session={session} />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
